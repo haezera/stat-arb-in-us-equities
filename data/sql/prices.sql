76acbee146c78c85b95468c63b9b44cd0371c50a
haeohreumkim <z5480978@ad.unsw.edu.au>
2025-07-26 12:04:44 +1000

    new: week to week backtest + ex-etf backtest

--- data/sql/prices.sql
+++ data/sql/prices.sql
@@ -5,6 +5,16 @@ create table adjusted_historical (
     high numeric(15, 4),
     low numeric(15, 4),
     open numeric(15, 4),
-    volume integer,
+    volume bigint,
+    ticker text
+);
+
+create table adjusted_week_to_week (
+    date date,
+    adj_close numeric(15, 4),
+    high numeric(15, 4),
+    low numeric(15, 4),
+    open numeric(15, 4),
+    volume bigint,
     ticker text
 );
